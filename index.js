877c787ecbd2dbe7ec74b0b69440f41e7889a7eb
Paweł Trysła <pawel.trysla@gmail.com>
2018-12-19 23:28:52 +0100

    update

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 module.exports = {
   extends: [
     'tslint-microsoft-contrib',
-    'tslint-config-prettier'
+    'tslint-config-prettier',
+    'tslint-react'
   ],
   rulesDirectory: [
     'tslint-plugin-prettier',
-    'tslint-eslint-rules'
   ],
   rules: {
-    'prettier': [true, { 'singleQuote': true }],
-    'function-name': [true, {
-      'static-method-regex': '^[a-z][\\w\\d]+$'
-    }],
-    'no-unsafe-any': { severity: 'warning' },
-    'no-default-export': false,
-    'no-relative-imports': false,
-    'export-name': false,
-    'promise-function-async': false,
-    'strict-boolean-expressions': false,
-    'match-default-export-name': false, 
-    'ordered-imports': false,
-    'object-literal-sort-keys': false,
-    'completed-docs': false,
-    'prefer-type-cast': false,
-    'typedef': [
+    // Base overrides
+    prettier: [true, { singleQuote: true, trailingComma: 'es5' }],
+    'array-type': [true, 'array-simple'],
+    "typedef": [
       true,
-      'parameter',
-      'arrow-parameter',
-      'property-declaration',
-      'member-variable-declaration'
-    ]
+      "parameter",
+      "arrow-parameter",
+      "property-declaration",
+      "member-variable-declaration"
+    ],
+
+    // MS contrib overrides
+    'arrow-return-shorthand': true,
+    'export-name': false,
+    "function-name": [true, {
+      "static-method-regex": "^[A-Za-z][\\w\\d]+$",
+      "function-regex": "^[A-Za-z][\\w\\d]+$"
+    }],
+    'import-name': false,
+    'max-func-body-length': false,
+    "member-access": [true, "no-public"],
+    "no-default-export": false,
+    "no-relative-imports": false,
+    "promise-function-async": false,
+    "strict-boolean-expressions": false,
+    "match-default-export-name": false, 
+    "ordered-imports": false,
+    "object-literal-sort-keys": false,
+    "completed-docs": false,
+    "newline-before-return": false,
+    "variable-name": [true, "ban-keywords", "allow-pascal-case"],
+    'prefer-type-cast': false,
+    "prefer-method-signature": false,
+    'no-use-before-declare': false,
+
+    // React overrides
+    'jsx-boolean-value': [true, 'never'],
+    'jsx-no-multiline-js': false,
   }
 }
---
+++ test/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function Body() {
+  return <span>Hello World</span>;
+}
+
+type Props = {
+  children: string;
+};
+
+type State = {
+  message: string;
+};
+
+export default class MyComponent extends React.Component<Props, State> {
+  state: State = {
+    message: '',
+  };
+
+  onClick = () => {
+    this.setState({ message: 'Goodbye world' });
+  };
+
+  render() {
+    return (
+      <>
+        <Body />
+        <div>{this.state.message}</div>
+        <button onClick={this.onClick}>Press me</button>
+      </>
+    );
+  }
+}
+
+export class MyClass {
+  readonly property1: boolean = true;
+
+  private property2?: string;
+
+  logProperty(times: number = 1) {
+    this.assignProperty();
+    for (let i = 0; i < times; i += 1) {
+      // tslint:disable-next-line: no-console
+      console.log(this.property2);
+    }
+  }
+
+  private assignProperty() {
+    this.property2 = 'Hey';
+  }
+}
